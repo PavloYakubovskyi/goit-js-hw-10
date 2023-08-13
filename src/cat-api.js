e5ce8365c6c4517891ff879cc1582d53ad9f4e02
Pavlo Yakubovskyi <pavloyakubovskyi@gmail.com>
2023-08-13 15:21:17 +0200

    added search cats

---
+++ src/cat-api.js
@@ -0,0 +1,73 @@
+function fetchBreeds() {
+  const BASE_URL = 'https://api.thecatapi.com/v1';
+  const END_POINT = '/breeds';
+  //   const API_KEY =
+  //     'live_n9RYsu6xXJu2x7fSIMilBDonMvDcz944KTSvauIhyOHhdhcDyRpeUJa4t2le08Oa';
+
+  //   const params = new URLSearchParams({
+  //     api_key: API_KEY,
+  //   });
+
+  const options = {
+    Headers: {
+      'x-api-key':
+        'live_n9RYsu6xXJu2x7fSIMilBDonMvDcz944KTSvauIhyOHhdhcDyRpeUJa4t2le08Oa',
+    },
+  };
+
+  return fetch(`${BASE_URL}${END_POINT}`, options).then(resp => {
+    if (!resp.ok) {
+      throw new Error(resp.statusText);
+    }
+
+    return resp.json();
+  });
+}
+
+function createBreedSelectMarkup(arr) {
+  return arr
+    .map(({ id, name }) => `<option value = "${id}" > ${name} </option>`)
+    .join('');
+}
+
+function fetchCatByBreed(breedId) {
+  const FORECAST_URL = 'https://api.thecatapi.com/v1/images/search';
+  const API_KEY =
+    'live_n9RYsu6xXJu2x7fSIMilBDonMvDcz944KTSvauIhyOHhdhcDyRpeUJa4t2le08Oa';
+
+  const params = new URLSearchParams({
+    breed_ids: breedId,
+  });
+
+  return fetch(`${FORECAST_URL}?api_key=${API_KEY}&${params}`).then(resp => {
+    if (!resp.ok) {
+      throw new Error(resp.statusText);
+    }
+
+    return resp.json();
+  });
+}
+
+function createCatMarkup({
+  0: {
+    breeds: {
+      0: { name, temperament, description },
+    },
+    url,
+  },
+}) {
+  // console.log(arr);
+  return `<img src="${url}" alt="${name}" width="800" height="500" />
+  <div>
+  <h1 class="title">${name}</h1>
+  <p class="description">${description}</p>
+  <h2>Temperament:</h2>
+  <p class="description">${temperament}</p></div>`;
+}
+
+export {
+  fetchBreeds,
+  fetchCatByBreed,
+  createBreedSelectMarkup,
+  createCatMarkup,
+};
--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
-import './css/styles.css';
+import {
+  fetchBreeds,
+  fetchCatByBreed,
+  createBreedSelectMarkup,
+  createCatMarkup,
+} from './cat-api';
 
-const DEBOUNCE_DELAY = 300;
+const refs = {
+  breedSelect: document.getElementById('breedSelect'),
+  catInfo: document.getElementById('catInfo'),
+};
+
+refs.breedSelect.addEventListener('change', onCatId);
+
+fetchBreeds().then(data => {
+  //   console.log(data[0]);
+  //   console.log(createBreedSelectMarkup(data));
+  refs.breedSelect.insertAdjacentHTML(
+    'beforeend',
+    createBreedSelectMarkup(data)
+  );
+});
+
+function onCatId(e) {
+  const id = e.target.value;
+  fetchCatByBreed(id)
+    .then(data => {
+      console.log(data);
+      return (refs.catInfo.innerHTML = createCatMarkup(data));
+    })
+    .catch(err => console.error(err));
+}
